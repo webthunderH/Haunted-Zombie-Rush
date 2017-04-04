1b6c7b1de408dcdbf4da18413e70d42fee44ee42
ThunderH <henry.dedonder@gmail.com>
2017-04-04 09:47:18 +0200

    Move the plateform
    Add a infinite movement on the platforme

---
+++ Assets/Scripts/ObjectMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMove : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 1.0f;
+	private float ResetPosition = -19.88f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.left * (speed *Time.deltaTime));
+		if (transform.localPosition.x <= ResetPosition)
+		{
+			Vector3 newPos = new Vector3(85.0f, transform.position.y, transform.position.z);
+			transform.position = newPos;
+		}
+	}
+}
