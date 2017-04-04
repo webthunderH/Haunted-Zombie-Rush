89b87fe06cb5ae581b5e3da877bf619e23a55c8d
ThunderH <henry.dedonder@gmail.com>
2017-04-04 11:23:30 +0200

    Add Rock
    Add rocks and Coroutine to get nice movement

--- Assets/Scripts/ObjectMove.cs
+++ Assets/Scripts/ObjectMove.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class ObjectMove : MonoBehaviour {
 
-	[SerializeField]
-	private float speed = 1.0f;
-	private float ResetPosition = -19.88f;
+	[SerializeField] float speed = 1.0f;
+	[SerializeField] float ResetPosition = -19.88f;
+	[SerializeField] float StartingPostion = 85.0f;
 	// Use this for initialization
 	void Start () {
 		
 	}
 
 	// Update is called once per frame
-	void Update () {
+	protected virtual void Update () {
 		transform.Translate(Vector3.left * (speed *Time.deltaTime));
 		if (transform.localPosition.x <= ResetPosition)
 		{
-			Vector3 newPos = new Vector3(85.0f, transform.position.y, transform.position.z);
+			Vector3 newPos = new Vector3(StartingPostion, transform.position.y, transform.position.z);
 			transform.position = newPos;
 		}
 	}
---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : ObjectMove {
+
+	[SerializeField] 
+	Vector3 topPosition;
+	[SerializeField]
+	Vector3 bottomPosition;
+	[SerializeField]
+	float SpeedUp;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(MoveRock(bottomPosition));
+	}
+
+	protected override void Update()
+	{
+		base.Update();
+	}
+	IEnumerator MoveRock(Vector3 targuet)
+	{
+		while (Mathf.Abs((targuet - transform.localPosition).y) > 0.20f)
+		{
+			Vector3 direction = targuet.y == topPosition.y ? Vector3.up : Vector3.down;
+			transform.localPosition += direction * (Time.deltaTime * SpeedUp);
+			yield return null;
+		}
+
+		Debug.Log("Targuet reached");
+
+		yield return new WaitForSeconds(0.5f);
+
+		Vector3 newTarguet = targuet.y == topPosition.y ? bottomPosition : topPosition;
+
+		StartCoroutine(MoveRock(newTarguet));
+	}
+}
